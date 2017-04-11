6f97a9f26f12c5e1dd2bcd6e6ce1790d87c72298
潘昕 <pansinm@163.com>
2017-04-11 11:10:08 +0800

    将接口暴露到index

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const invoke = require('./lib/invoke');
+const message = require('./lib/message');
 
+/**
+ * @module node-rongcloud
+ */
+
+/**
+ * 初始化
+ * @function init
+ * @static
+ * @param {object} options
+ * @param {string} options.appKey
+ * @param {string} options.appSecret
+ * @param {boolean} options.usePrefix 头部是否使用RC-前缀
+ */
+module.exports.init = invoke.init;
+
+/**
+ * 融云请求接口
+ * @see {@link module:invoke}
+ * @type {invoke}
+ */
+module.exports.invoke = invoke;
+
+/**
+ * 消息相关接口
+ * @see {@link module:message}
+ * @type {message}
+ */
+module.exports.message = message;
