9ab379177f36869918885db177c4ade54f34e687
潘昕 <pansinm@163.com>
2017-04-15 17:26:39 +0800

    增加系统消息发送接口

--- test/message.test.js
+++ test/message.test.js
@@ -9,7 +9,7 @@ const rongCloud = new RongCloud({
 });
 
 describe('Message Test', () => {
-  describe('Publish Message', () => {
+  describe('Publish Private Message', () => {
     it('Publish TextMessage', (done) => {
       rongCloud.publishPrivateMessage({
         toUserId: config.message.toUserId,
@@ -22,6 +22,22 @@ describe('Message Test', () => {
           done();
         })
         .catch(done);
+    });
+  });
+
+  describe('Publish System Message', () => {
+    it('Publish Text Message', (done) => {
+      rongCloud.publishSystemMessage({
+        toUserId: config.message.toUserId,
+        fromUserId: config.message.fromUserId,
+        content: JSON.stringify({ content: config.message.textMsg }),
+        objectName: 'RC:TxtMsg'
+      })
+        .then((result) => {
+          expect(result).to.have.property('code', 200);
+          done();
+        })
+        .catch(done)
     });
   });
 });
